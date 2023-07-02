cc1579c4f498c6c5320e2cd82801a7c10d511960
berkeleycole <alyssa@freshbrewlabs.com>
2023-07-02 16:45:28 -0700

    postgres tests - authorities

--- oxidauth-postgres/src/authorities/insert_authority/mod.rs
+++ oxidauth-postgres/src/authorities/insert_authority/mod.rs
@@ -24,3 +24,40 @@ impl InsertAuthority for Database {
         Ok(result)
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use sqlx::PgPool;
+
+    use super::*;
+
+    #[sqlx::test]
+    async fn it_should_insert_an_authority_successfully(pool: PgPool) {
+        let db = Database { pool };
+
+        let authority_id = Uuid::new_v4();
+
+        let insert_params = InsertAuthorityParams {
+            id: Some(authority_id),
+            name: "Test".to_string(),
+            client_key: Uuid::new_v4(),
+            status: "Test".to_string(),
+            strategy: "Test".to_string(),
+            settings: serde_json::Value::default(),
+            params: serde_json::Value::default(),
+        };
+
+        match db.insert_authority(&insert_params).await {
+            Ok(authority) => {
+                assert_eq!(authority_id, authority.id);
+                assert_eq!(insert_params.name, authority.name);
+                assert_eq!(insert_params.client_key, authority.client_key);
+                assert_eq!(insert_params.status, authority.status);
+                assert_eq!(insert_params.strategy, authority.strategy);
+                assert_eq!(insert_params.settings, authority.settings);
+                assert_eq!(insert_params.params, authority.params);
+            }
+            Err(_) => unreachable!(),
+        }
+    }
+}
