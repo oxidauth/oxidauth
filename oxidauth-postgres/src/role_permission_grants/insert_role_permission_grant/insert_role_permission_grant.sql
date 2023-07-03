2326c162b814b8d6680ad223286406b1dde802e1
berkeleycole <alyssa@freshbrewlabs.com>
2023-07-02 20:52:59 -0700

    postgres tests - role permission grants

--- oxidauth-postgres/src/role_permission_grants/insert_role_permission_grant/insert_role_permission_grant.sql
+++ oxidauth-postgres/src/role_permission_grants/insert_role_permission_grant/insert_role_permission_grant.sql
@@ -1,4 +1,4 @@
 INSERT INTO role_permission_grants
-(role_id, permission_id, created_at, updated_at)
-VALUES ($1, $2, NOW(), NOW())
+(id, role_id, permission_id, created_at, updated_at)
+VALUES (COALESCE($1, generate_uuid_v4()), $2, $3, NOW(), NOW())
 RETURNING *
