932199c382e29f16b9beff212c9bba93ad330e43
berkeleycole <alyssa@freshbrewlabs.com>
2023-07-02 20:32:14 -0700

    postgres tests - public keys

--- oxidauth-postgres/src/public_keys/insert_public_key/mod.rs
+++ oxidauth-postgres/src/public_keys/insert_public_key/mod.rs
@@ -10,6 +10,7 @@ impl InsertPublicKey for Database {
     ) -> Result<PublicKeyRow, InsertPublicKeyError> {
         let result =
             sqlx::query_as::<_, super::PublicKeyRow>(include_str!("./insert_public_key.sql"))
+                .bind(&params.id)
                 .bind(&params.private_key)
                 .bind(&params.public_key)
                 .fetch_one(&self.pool)
@@ -18,5 +19,36 @@ impl InsertPublicKey for Database {
                 .map_err(|_| InsertPublicKeyError {})?;
 
         Ok(result)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use oxidauth_repository::public_keys::insert_public_key::*;
+    use sqlx::PgPool;
+
+    use super::*;
+
+    #[sqlx::test]
+    async fn it_should_a_public_key_successfully(pool: PgPool) {
+        let db = Database { pool };
+
+        let public_key_id = Uuid::new_v4();
+
+        // @GEORGE - again just guessing
+        let insert_params = InsertPublicKeyParams {
+            id: Some(public_key_id),
+            public_key: [1, 1, 1, 1].to_vec(),
+            private_key: [2, 2, 2, 2].to_vec(),
+        };
+
+        match db.insert_public_key(&insert_params).await {
+            Ok(public_key) => {
+                assert_eq!(public_key_id, public_key.id);
+                assert_eq!(insert_params.public_key, public_key.public_key);
+                assert_eq!(insert_params.private_key, public_key.private_key);
+            }
+            Err(_) => unreachable!(),
+        }
     }
 }
--- oxidauth-postgres/src/public_keys/query_public_key_by_id/mod.rs
+++ oxidauth-postgres/src/public_keys/query_public_key_by_id/mod.rs
@@ -19,3 +19,36 @@ impl QueryPublicKeyById for Database {
         Ok(result)
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use oxidauth_repository::public_keys::insert_public_key::*;
+    use sqlx::PgPool;
+
+    use super::*;
+
+    #[sqlx::test]
+    async fn it_should_query_a_public_key_by_id_successfully(pool: PgPool) {
+        let db = Database { pool };
+
+        let public_key_id = Uuid::new_v4();
+
+        let insert_params = InsertPublicKeyParams {
+            id: Some(public_key_id),
+            public_key: [1, 1, 1, 1].to_vec(),
+            private_key: [2, 2, 2, 2].to_vec(),
+        };
+
+        db.insert_public_key(&insert_params)
+            .await
+            .expect("should be able to insert public_key");
+
+        match db.query_public_key_by_id(public_key_id).await {
+            Ok(public_key) => {
+                assert_eq!(public_key_id, public_key.id);
+                assert_eq!(insert_params.public_key, public_key.public_key);
+            }
+            Err(_) => unreachable!(),
+        }
+    }
+}
