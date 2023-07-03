76a717e97837d7b11ebb8db2b348590522621396
berkeleycole <alyssa@freshbrewlabs.com>
2023-07-02 21:05:17 -0700

    postgres tests - roles

--- oxidauth-postgres/src/roles/update_role/mod.rs
+++ oxidauth-postgres/src/roles/update_role/mod.rs
@@ -16,3 +16,40 @@ impl UpdateRole for Database {
         Ok(result)
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use oxidauth_repository::roles::insert_role::*;
+    use sqlx::PgPool;
+
+    use super::*;
+
+    #[sqlx::test]
+    async fn it_should_update_a_role_successfully(pool: PgPool) {
+        let db = Database { pool };
+
+        let role_id = Uuid::new_v4();
+
+        let insert_params = InsertRoleParams {
+            id: Some(role_id),
+            name: "Test".to_string(),
+        };
+
+        let update_params = UpdateRoleParams {
+            id: role_id,
+            name: "UpdateName".to_string(),
+        };
+
+        db.insert_role(&insert_params)
+            .await
+            .expect("should be able to insert role");
+
+        match db.update_role(&update_params).await {
+            Ok(role) => {
+                assert_eq!(role_id, role.id);
+                assert_eq!(insert_params.name, role.name);
+            }
+            Err(_) => unreachable!(),
+        }
+    }
+}
