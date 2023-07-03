7a1189d3e9f927ef1d28eed7b4ea04a9e2b1be2f
berkeleycole <alyssa@freshbrewlabs.com>
2023-07-03 10:36:24 -0700

    postgres tests - users

--- oxidauth-postgres/src/users/query_user_by_id/mod.rs
+++ oxidauth-postgres/src/users/query_user_by_id/mod.rs
@@ -15,3 +15,44 @@ impl QueryUserById for Database {
         Ok(result)
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use oxidauth_repository::users::insert_user::*;
+    use sqlx::PgPool;
+
+    use super::*;
+
+    #[sqlx::test]
+    async fn it_should_query_a_user_by_id_successfully(pool: PgPool) {
+        let db = Database { pool };
+
+        let user_id = Uuid::new_v4();
+
+        let insert_params = InsertUserParams {
+            id: Some(user_id),
+            kind: Some("Test".to_string()),
+            status: Some("Test".to_string()),
+            username: "Test".to_string(),
+            email: Some("test@test.com".to_string()),
+            first_name: Some("TestFirst".to_string()),
+            last_name: Some("TestLast".to_string()),
+            profile: Some(serde_json::Value::default()),
+        };
+
+        db.insert_user(&insert_params)
+            .await
+            .expect("should be able to insert user");
+
+        match db.query_user_by_id(user_id).await {
+            Ok(user) => {
+                assert_eq!(user_id, user.id);
+                assert_eq!(insert_params.last_name, user.last_name);
+                assert_eq!(insert_params.kind, user.kind);
+                assert_eq!(insert_params.email, user.email);
+                assert_eq!(insert_params.username, user.username);
+            }
+            Err(_) => unreachable!(),
+        }
+    }
+}
