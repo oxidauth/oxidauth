6893ca80e8e0794a77c580beb935a2cc6cd20b29
berkeleycole <alyssa@freshbrewlabs.com>
2023-07-02 20:44:37 -0700

    postgres tests - refresh tokens

--- oxidauth-postgres/src/refresh_tokens/insert_refresh_token/mod.rs
+++ oxidauth-postgres/src/refresh_tokens/insert_refresh_token/mod.rs
@@ -10,6 +10,7 @@ impl InsertRefreshToken for Database {
     ) -> Result<RefreshTokenRow, InsertRefreshTokenError> {
         let result =
             sqlx::query_as::<_, super::RefreshTokenRow>(include_str!("./insert_refresh_token.sql"))
+                .bind(&params.id)
                 .bind(&params.user_id)
                 .bind(&params.authority_id)
                 .bind(&params.expires_at)
@@ -19,5 +20,39 @@ impl InsertRefreshToken for Database {
                 .map_err(|_| InsertRefreshTokenError {})?;
 
         Ok(result)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use sqlx::PgPool;
+
+    use super::*;
+
+    #[sqlx::test]
+    async fn it_should_insert_a_refresh_token_successfully(pool: PgPool) {
+        let db = Database { pool };
+
+        let refresh_token_id = Uuid::new_v4();
+        let user_id = Uuid::new_v4();
+        let authority_id = Uuid::new_v4();
+
+        // @GEORGE - not sure abou the expires at
+        let insert_params = InsertRefreshTokenParams {
+            id: Some(refresh_token_id),
+            user_id: user_id,
+            authority_id: authority_id,
+            expires_at: Utc::now(),
+        };
+
+        match db.insert_refresh_token(&insert_params).await {
+            Ok(refresh_token) => {
+                assert_eq!(refresh_token_id, refresh_token.id);
+                assert_eq!(user_id, refresh_token.user_id);
+                assert_eq!(authority_id, refresh_token.authority_id);
+                assert_eq!(insert_params.expires_at, refresh_token.expires_at);
+            }
+            Err(_) => unreachable!(),
+        }
     }
 }
