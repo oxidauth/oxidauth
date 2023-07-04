61da28ca5552843460d224eb337ef7668307a051
George Wheeler <george@freshbrewlabs.com>
2023-07-04 13:50:33 -0700

    remove oxidauth-server from workspace and handle independently and setup cross compiling

--- oxidauth-server/src/main.rs
+++ oxidauth-server/src/main.rs
@@ -16,13 +16,19 @@ pub type BoxedError = Box<(dyn Error + Send + Sync + 'static)>;
 
 #[tokio::main]
 async fn main() -> Result<(), BoxedError> {
-    dotenv::dotenv()?;
+    // dotenv::dotenv()?;
+    println!("starting oxidauth-server");
 
-    let bind_addr = var("BIND_ADDR")?.parse()?;
-    let database_url = var("DATABASE_URL")?;
+    let bind_addr = var("BIND_ADDR").expect("should have BIND_ADDR").parse()?;
+    let database_url = var("DATABASE_URL").expect("should have DATABASE_URL");
 
+    println!("all env vars accounted for");
+
+    println!("setting up database connection");
     let database = PgPoolOptions::new().connect(&database_url).await?;
+    println!("database setup");
 
+    println!("starting http server");
     axum::Server::new()
         .address(bind_addr)
         .database(database)
