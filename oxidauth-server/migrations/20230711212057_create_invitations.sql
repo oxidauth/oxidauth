3d2c594e4b1eef2220cd0b65e355f5c5bb5dd8eb
George Wheeler <george@freshbrewlabs.com>
2023-07-12 21:49:46 -0700

    fix migration

--- oxidauth-server/migrations/20230711212057_create_invitations.sql
+++ oxidauth-server/migrations/20230711212057_create_invitations.sql
@@ -3,7 +3,7 @@ CREATE TABLE invitations (
     user_id UUID NOT NULL,
     expires_at TIMESTAMPTZ NOT NULL,
     created_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE INDEX refresh_tokens_user_id ON invitations(user_id);
