ca7a583096641c8109e02e8fb2177ea3e1f7fcc6
George Wheeler <george@freshbrewlabs.com>
2023-12-26 10:29:44 -0800

    progress

--- oxidauth-server/build.sh
+++ oxidauth-server/build.sh
@@ -17,10 +17,16 @@ cross build --target x86_64-unknown-linux-gnu --release
 
 cp $CARGO_TARGET_DIR/x86_64-unknown-linux-gnu/release/oxidauth-server tmp/oxidauth-server 
 
-VERSION=$(cat Cargo.toml | grep version | head -1 | cut -d'"' -f 2 | tr -d '\n')
-IMAGE=registry.vizerapp.cloud/pub/oxidauth-server:$VERSION
+docker buildx create --name oxidauth-builder --use
 
-docker buildx create --name oxidauth-test --use
-docker buildx build --platform=linux/amd64 -t $IMAGE --push -f Dockerfile . 
+VERSION=$(cat Cargo.toml | grep version | head -1 | cut -d'"' -f 2 | tr -d '\n')
+CROSS_CONTAINER_ENGINE_NO_BUILDKIT=1
+PLATFORMS=linux/amd64,linux/arm64
+
+IMAGE=registry.vizerapp.cloud/pub/oxidauth-server:$VERSION
+docker buildx build --platform=$PLATFORMS -t $IMAGE --push -f Dockerfile .
+
+IMAGE=registry.vizerapp.cloud/prism/oxidauth-server:$VERSION
+docker buildx build --platform=$PLATFORMS -t $IMAGE --push -f Dockerfile . 
 
 rm tmp/oxidauth-server
