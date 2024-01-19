8cd3659eb86a098398c3723ba8def4bff500550c
Drew Bradford <drewbrad4@gmail.com>
2024-01-19 09:15:00 -0800

    adds user_authorities by user_id

---
+++ oxidauth-postgres/src/user_authorities/select_user_authorities_by_user_id/select_user_authorities_by_user_id.sql
@@ -0,0 +1,18 @@
+SELECT
+    user_authorities.user_id,
+    user_authorities.authority_id,
+    user_authorities.user_identifier,
+    user_authorities.params,
+    user_authorities.created_at,
+    user_authorities.updated_at,
+    authorities.name AS authority_name,
+    authorities.client_key AS authority_client_key,
+    authorities.status AS authority_status,
+    authorities.strategy AS authority_strategy,
+    authorities.settings AS authority_settings,
+    authorities.params AS authority_params,
+    authorities.created_at AS authority_created_at,
+    authorities.updated_at AS authority_updated_at
+FROM user_authorities
+JOIN authorities ON user_authorities.authority_id = authorities.id
+WHERE user_authorities.user_id = $1
