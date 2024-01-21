39ced466c4991badd34ace6c52c4c62bfbdeb671
George Wheeler <george@freshbrewlabs.com>
2024-01-21 14:20:41 -0800

    tiny change

--- oxidauth-kernel/src/auth/mod.rs
+++ oxidauth-kernel/src/auth/mod.rs
@@ -1,21 +1,19 @@
 pub mod tree;
 
-pub mod authority {
-    use async_trait::async_trait;
+use async_trait::async_trait;
 
-    use crate::authorities::UserAuthority;
+use crate::authorities::UserAuthority;
 
-    #[async_trait]
-    pub trait Registrar<Params>: Send + Sync + 'static {
-        async fn register(&self, params: Params);
-    }
+#[async_trait]
+pub trait Registrar<Params>: Send + Sync + 'static {
+    async fn register(&self, params: Params);
+}
 
-    #[async_trait]
-    pub trait Authenticator<Params>: Send + Sync + 'static {
-        async fn authenticate(
-            &self,
-            params: Params,
-            user_authority: &UserAuthority,
-        );
-    }
+#[async_trait]
+pub trait Authenticator<Params>: Send + Sync + 'static {
+    async fn authenticate(
+        &self,
+        params: Params,
+        user_authority: &UserAuthority,
+    );
 }
