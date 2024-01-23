699ad4d88ce47b5811c91acb772a495e4ad1205a
George Wheeler <george@freshbrewlabs.com>
2024-01-22 16:23:50 -0800

    split registrar trait up

--- oxidauth-kernel/src/auth/mod.rs
+++ oxidauth-kernel/src/auth/mod.rs
@@ -4,11 +4,22 @@ pub mod tree;
 use async_trait::async_trait;
 use serde_json::Value;
 
-use crate::{authorities::UserAuthority, dev_prelude::BoxedError};
+use crate::{
+    authorities::UserAuthority, dev_prelude::BoxedError,
+    user_authorities::user_authority_create::UserAuthorityCreate,
+};
 
 #[async_trait]
-pub trait Registrar: UserIdentifierFromRequest + Send + Sync + 'static {
+pub trait Registrar: UserAuthorityFromRequest + Send + Sync + 'static {
     async fn register(&self, params: Value);
+}
+
+#[async_trait]
+pub trait UserAuthorityFromRequest: Send + Sync + 'static {
+    async fn user_authority_from_request(
+        &self,
+        params: Value,
+    ) -> Result<UserAuthorityCreate, BoxedError>;
 }
 
 #[async_trait]
