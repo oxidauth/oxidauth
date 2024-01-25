41dd9e233ecfb8a02ead80c5af517973eac7ae02
Drew Bradford <drewbrad4@gmail.com>
2024-01-25 14:41:25 -0800

    unmerges imports

--- oxidauth-http/src/server/api/v1/users/find_user_by_id.rs
+++ oxidauth-http/src/server/api/v1/users/find_user_by_id.rs
@@ -8,11 +8,11 @@ use oxidauth_permission::parse_and_validate_multiple;
 use serde::Serialize;
 use tracing::{info, warn};
 
-use crate::response::Response;
-use crate::{
-    middleware::permission_extractor::{ExtractEntitlements, ExtractJwt},
-    provider::Provider,
+use crate::middleware::permission_extractor::{
+    ExtractEntitlements, ExtractJwt,
 };
+use crate::provider::Provider;
+use crate::response::Response;
 
 pub type FindUserByIdReq = FindUserById;
 
