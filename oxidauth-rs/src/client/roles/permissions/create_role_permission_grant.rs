0fafc7642c1c60022d49af7a98ca357af30b1822
Drew Bradford <drewbrad4@gmail.com>
2024-01-29 15:09:45 -0800

    makes methods pub

--- oxidauth-rs/src/client/roles/permissions/create_role_permission_grant.rs
+++ oxidauth-rs/src/client/roles/permissions/create_role_permission_grant.rs
@@ -1,8 +1,6 @@
-use oxidauth_http::{
-    response::Response,
-    server::api::v1::roles::permissions::create_role_permission_grant::{
-        CreateRolePermissionGrantReq, CreateRolePermissionGrantRes,
-    },
+use oxidauth_http::response::Response;
+pub use oxidauth_http::server::api::v1::roles::permissions::create_role_permission_grant::{
+    CreateRolePermissionGrantReq, CreateRolePermissionGrantRes,
 };
 use oxidauth_kernel::error::BoxedError;
 
@@ -12,7 +10,7 @@ const RESOURCE: Resource = Resource::RolePermissionGrant;
 const METHOD: &str = "create_role_permission_grant";
 
 impl Client {
-    async fn create_role_permission_grant<T>(
+    pub async fn create_role_permission_grant<T>(
         &self,
         role_permission_grant: T,
     ) -> Result<CreateRolePermissionGrantRes, BoxedError>
@@ -28,7 +26,7 @@ impl Client {
                     role_permission_grant.role_id,
                     role_permission_grant.permission
                 ),
-                None::<()>,
+                None::<CreateRolePermissionGrantReq>,
             )
             .await?;
 
