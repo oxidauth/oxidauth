3163d7099242dce747b49a284e3b02ada7c0e494
JD Bailey <jdjr.bailey@gmail.com>
2024-01-29 14:22:02 -0800

    adding exchange refresh token method to oxidauth-rs

---
+++ oxidauth-rs/src/client/refresh_tokens/exchange_refresh_token.rs
@@ -0,0 +1,37 @@
+use oxidauth_http::{
+    response::Response,
+    server::api::v1::refresh_tokens::exchange::{ExchangeRefreshTokenReq, ExchangeRefreshTokenRes},
+};
+use oxidauth_kernel::error::BoxedError;
+
+use super::*;
+
+const RESOURCE: Resource = Resource::RefreshToken;
+const METHOD: &str = "exchange_refresh_token";
+
+impl Client {
+    async fn exchange_refresh_token<T>(
+        &self,
+        params: T,
+    ) -> Result<ExchangeRefreshTokenRes, BoxedError>
+    where
+        T: Into<ExchangeRefreshTokenReq>,
+    {
+        let params = params.into();
+
+        let resp: Response<ExchangeRefreshTokenRes> = self
+            .post(
+                "/refresh_tokens",
+                params,
+            )
+            .await?;
+
+        let role_res = handle_response(
+            RESOURCE,
+            METHOD,
+            resp,
+        )?;
+
+        Ok(role_res)
+    }
+}
---
+++ oxidauth-rs/src/client/refresh_tokens/mod.rs
@@ -0,0 +1,3 @@
+mod exchange_refresh_token;
+
+use super::{handle_response, Client, ClientError, ClientErrorKind, Resource};
