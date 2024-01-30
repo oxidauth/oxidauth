f59509ec83f7ec683664c1584549bf73b2e35b19
Drew Bradford <drewbrad4@gmail.com>
2024-01-30 11:24:20 -0800

    removes useless struct in favor of bool

--- oxidauth-rs/src/client/can/mod.rs
+++ oxidauth-rs/src/client/can/mod.rs
@@ -1,5 +1,5 @@
 use oxidauth_http::response::Response;
-pub use oxidauth_http::server::api::v1::can::{CanReq, CanRes};
+pub use oxidauth_http::server::api::v1::can::CanReq;
 use oxidauth_kernel::error::BoxedError;
 
 use super::*;
@@ -8,13 +8,13 @@ const RESOURCE: Resource = Resource::Permission;
 const METHOD: &str = "can";
 
 impl Client {
-    pub async fn can<T>(&self, params: T) -> Result<CanRes, BoxedError>
+    pub async fn can<T>(&self, params: T) -> Result<bool, BoxedError>
     where
         T: Into<CanReq>,
     {
         let params = params.into();
 
-        let resp: Response<CanRes> = self
+        let resp: Response<bool> = self
             .get(
                 &format!("/can/{}", params.permission),
                 None::<CanReq>,
