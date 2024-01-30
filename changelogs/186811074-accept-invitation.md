fe444863d2d1f74924c6f365934fe7503b4e11db
George Wheeler <george@freshbrewlabs.com>
2024-01-29 18:31:55 -0800

    progress

---
+++ changelogs/186811074-accept-invitation.md
@@ -0,0 +1,5 @@
+- [186811074](https://www.pivotaltracker.com/story/show/186811074) - Accept Invitation 
+    - changes user.username to be updated to allow for invitations to work better
+    - changes create invitation to allow the setting up of the entire user
+    - adds accept invitation to stack 
+
---
+++ oxidauth-kernel/src/invitations/accept_invitation.rs
@@ -0,0 +1,51 @@
+use std::sync::Arc;
+
+use serde::{Deserialize, Serialize};
+use serde_json::Value;
+use uuid::Uuid;
+
+use crate::{
+    dev_prelude::{BoxedError, Service},
+    users::{update_user::UpdateUser, User, UserStatus},
+};
+
+pub type AcceptInvitationService = Arc<
+    dyn for<'a> Service<
+        &'a AcceptInvitationParams,
+        Response = User,
+        Error = BoxedError,
+    >,
+>;
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct AcceptInvitationParams {
+    pub invitation_id: Uuid,
+    pub user: AcceptInvitationUserParams,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct AcceptInvitationUserParams {
+    pub id: Option<Uuid>,
+    pub username: String,
+    pub email: Option<String>,
+    pub first_name: Option<String>,
+    pub last_name: Option<String>,
+    pub status: Option<UserStatus>,
+    pub profile: Option<Value>,
+}
+
+impl From<&AcceptInvitationUserParams> for UpdateUser {
+    fn from(value: &AcceptInvitationUserParams) -> Self {
+        let value = value.clone();
+
+        Self {
+            id: value.id,
+            username: Some(value.username),
+            email: value.email,
+            first_name: value.first_name,
+            last_name: value.last_name,
+            status: value.status,
+            profile: value.profile,
+        }
+    }
+}
