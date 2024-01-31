7d4a7c30fcf5778f938265c59b264592e3e96f01
Drew Bradford <drewbrad4@gmail.com>
2024-01-31 14:19:22 -0800

    removes old methods from users

--- oxidauth-postgres/src/users/mod.rs
+++ oxidauth-postgres/src/users/mod.rs
@@ -7,7 +7,6 @@ pub use oxidauth_kernel::users::{ParseUserKindErr, ParseUserStatusErr, User};
 
 pub mod delete_user_by_id_query;
 pub mod insert_user;
-pub mod query_user_by_email;
 pub mod select_all_users_query;
 pub mod select_user_by_id_query;
 pub mod select_user_by_username_query;
--- oxidauth-postgres/src/users/query_user_by_email/query_user_by_email.sql
+++
@@ -1,3 +0,0 @@
-SELECT *
-FROM users
-WHERE email = $1
