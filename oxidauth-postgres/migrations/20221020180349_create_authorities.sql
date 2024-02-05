433df4a7c6fecbcc58b16304e1fc936674716ae4
George Wheeler <george@freshbrewlabs.com>
2024-02-04 19:19:17 -0800

    change postgres size limit for roles.names and authorities.strategies to 256

--- oxidauth-postgres/migrations/20221020180349_create_authorities.sql
+++ oxidauth-postgres/migrations/20221020180349_create_authorities.sql
@@ -3,7 +3,7 @@ CREATE TABLE authorities (
     name VARCHAR(255) UNIQUE NOT NULL,
     client_key UUID UNIQUE DEFAULT uuid_generate_v4(),
     status VARCHAR(32) NOT NULL,
-    strategy VARCHAR(32) NOT NULL UNIQUE,
+    strategy VARCHAR(64) NOT NULL UNIQUE,
     settings JSONB DEFAULT '{}'::jsonb,
     params JSONB DEFAULT '{}'::jsonb,
     created_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
