2e1f001eebf9852cb04c4283b608b2256a5a4f60
berkeleycole <alyssa@freshbrewlabs.com>
2024-04-17 16:06:27 -0700

    revamp migration

---
+++ oxidauth-postgres/migrations/20240416170202_create_auth_keys.sql
@@ -0,0 +1,9 @@
+CREATE TABLE auth_keys (
+    id UUID PRIMARY KEY NOT NULL DEFAULT uuid_generate_v4(),
+    user_id UUID FOREIGN KEY REFERENCES Users(id) NOT NULL,
+    code VARCHAR(256) UNIQUE NOT NULL,
+    created_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE INDEX auth_keys_user_id ON auth_keys(user_id);
--- oxidauth-postgres/migrations/20240416170202_create_email_codes.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE email_codes (
-    id UUID PRIMARY KEY NOT NULL DEFAULT uuid_generate_v4(),
-    user_id UUID NOT NULL,
-    code VARCHAR(256) UNIQUE NOT NULL,
-    expires_at TIMESTAMPTZ NOT NULL,
-    created_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
-);
-
-CREATE INDEX email_code_user_id ON email_codes(user_id);
