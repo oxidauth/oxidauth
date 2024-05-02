4196b97a887bc910d08b21a34dea0ee2f26b36a5
berkeleycole <alyssa@freshbrewlabs.com>
2024-05-01 20:12:48 -0700

    after some additional pondering, changing the secret key storage type

--- oxidauth-postgres/migrations/20240416170202_create_totp_secrets.sql
+++ oxidauth-postgres/migrations/20240416170202_create_totp_secrets.sql
@@ -1,7 +1,7 @@
 CREATE TABLE totp_secrets (
     id UUID PRIMARY KEY NOT NULL DEFAULT uuid_generate_v4(),
     user_id UUID FOREIGN KEY REFERENCES Users(id) NOT NULL ON UPDATE cascade ON DELETE cascade,
-    totp_secret INTEGER[] NOT NULL,
+    totp_secret BIGINT NOT NULL,
     created_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
