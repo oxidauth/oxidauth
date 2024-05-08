53767490b596bad85f3ec495bbf6d37eea6981d3
berkeleycole <alyssa@freshbrewlabs.com>
2024-05-08 12:04:18 -0700

    validate logic

--- oxidauth-kernel/src/totp/validate.rs
+++ oxidauth-kernel/src/totp/validate.rs
@@ -2,6 +2,12 @@ use crate::dev_prelude::*;
 
 use super::TOTPValidation;
 
+#[derive(Debug, Serialize, Deserialize)]
+pub struct ValidateTOTPReq {
+    pub user_id: Uuid,
+    pub code: u32,
+}
+
 pub type ValidateTOTPService = Arc<
     dyn for<'a> Service<
         &'a ValidateTOTP,
