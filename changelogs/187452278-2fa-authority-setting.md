e431d01e231f4930e542028d9db06e593ce819b3
alyssamhope <berkeleycole@users.noreply.github.com>
2024-06-20 17:52:31 -0700

    Apply suggestions from code review

--- changelogs/187452278-2fa-authority-setting.md
+++ changelogs/187452278-2fa-authority-setting.md
@@ -1,1 +1,2 @@
 - [187452278](https://www.pivotaltracker.com/story/show/187452278) - adds 2FA setting to authority
+
--- oxidauth-postgres/migrations/20240619063635_modify_user_authority_user_identifier_unique.sql
+++ oxidauth-postgres/migrations/20240619063635_modify_user_authority_user_identifier_unique.sql
@@ -1,4 +1,5 @@
-ALTER TABLE user_authorities DROP CONSTRAINT user_authorities_user_identifier_key;
+ALTER TABLE user_authorities
+DROP CONSTRAINT user_authorities_user_identifier_key;
 
 ALTER TABLE user_authorities
 ADD UNIQUE (user_identifier, authority_id);
