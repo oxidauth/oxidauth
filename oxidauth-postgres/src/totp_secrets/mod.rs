17c50a4e767c4dd7f4cea95f38609700ceb4f1a1
berkeleycole <alyssa@freshbrewlabs.com>
2024-06-20 18:01:11 -0700

    remove needless duplicate struct

--- oxidauth-postgres/src/totp_secrets/mod.rs
+++ oxidauth-postgres/src/totp_secrets/mod.rs
@@ -6,28 +6,10 @@ pub mod select_where_no_totp_secret_by_authority_id;
 use chrono::{DateTime, Utc};
 use uuid::Uuid;
 
-pub struct TOTPSecretRow {
-    pub id: Uuid,
-    pub user_id: Uuid,
-    pub totp_secret: String,
-    pub created_at: DateTime<Utc>,
-}
-
 #[derive(Debug, sqlx::FromRow)]
 pub struct PgTotpSecret {
     pub id: Uuid,
     pub user_id: Uuid,
     pub totp_secret: String,
     pub created_at: DateTime<Utc>,
-}
-
-impl From<PgTotpSecret> for TOTPSecretRow {
-    fn from(value: PgTotpSecret) -> Self {
-        Self {
-            id: value.id,
-            user_id: value.user_id,
-            totp_secret: value.totp_secret,
-            created_at: value.created_at,
-        }
-    }
 }
