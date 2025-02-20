40a9054ab97b99cae8081bcdc53c18e6db012124
berkeleycole <alyssa@freshbrewlabs.com>
2025-02-19 16:15:46 -0800

    re export entitlements encoding

--- oxidauth-rs/src/prelude.rs
+++ oxidauth-rs/src/prelude.rs
@@ -1,3 +1,4 @@
 pub use oxidauth_http::prelude::parse_and_validate;
 pub use oxidauth_kernel::error::BoxedError;
+pub use oxidauth_kernel::jwt::EntitlementsEncoding;
 pub use oxidauth_kernel::jwt::Jwt;
