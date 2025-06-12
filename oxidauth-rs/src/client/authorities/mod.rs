ed2809d56b205daf111c67576fbaf4387a7aee9c
berkeleycole <alyssa@freshbrewlabs.com>
2025-06-12 16:03:49 -0700

    cleanup

--- oxidauth-rs/src/client/authorities/mod.rs
+++ oxidauth-rs/src/client/authorities/mod.rs
@@ -5,6 +5,7 @@ pub mod find_authority_by_strategy;
 pub mod list_all_authorities;
 pub mod update_authority;
 
+pub use oxidauth_kernel::authorities::NbfOffset;
 pub use oxidauth_kernel::authorities::create_authority::*;
 
 use super::{Client, Resource, fmt, handle_response};
