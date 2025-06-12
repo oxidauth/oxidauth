e955022d83da2145cadc0a0e20b8c05b1681b476
berkeleycole <alyssa@freshbrewlabs.com>
2025-06-12 16:11:17 -0700

    removing logs

--- oxidauth-usecases/src/auth/strategies/oauth2/redirect.rs
+++ oxidauth-usecases/src/auth/strategies/oauth2/redirect.rs
@@ -6,7 +6,6 @@ use oxidauth_kernel::{
     error::BoxedError,
 };
 use oxidauth_repository::authorities::select_authority_by_client_key::SelectAuthorityByClientKeyQuery;
-use tracing_log::log::info;
 
 use super::{AuthorityParams, OAuthFlavors};
 
@@ -36,8 +35,6 @@ where
 
     #[tracing::instrument(name = "find_authority_by_client_key_usecase", skip(self))]
     async fn call(&self, params: &'a Oauth2RedirectParams) -> Result<Self::Response, Self::Error> {
-        info!("Got to redirect call");
-
         let authority = self
             .authorities
             .call(&FindAuthorityByClientKey {
@@ -46,11 +43,7 @@ where
             .await?
             .ok_or_else(|| AuthorityNotFoundError::client_key(params.client_key))?;
 
-        info!("Got authority");
-
         let oauth_params: AuthorityParams = authority.params.try_into()?;
-
-        info!("Can get oauth params, {:?}", oauth_params);
 
         let redirect_url = match oauth_params.flavor {
             OAuthFlavors::Google => {
