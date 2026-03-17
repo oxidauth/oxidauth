9a8ae8755dbc472a96d7e5e1f751dbcb88350336
Drew Bradford <drewbrad4@gmail.com>
2026-03-17 12:45:04 -0700

    WIP

--- oxidauth-rs/src/client/users/list_all_users.rs
+++ oxidauth-rs/src/client/users/list_all_users.rs
@@ -1,13 +1,87 @@
+use std::sync::Arc;
+
 use oxidauth_http::response::Response;
 pub use oxidauth_http::server::api::v1::users::list_all_users::{
     ListAllUsersReq, ListAllUsersRes,
 };
 use oxidauth_kernel::error::BoxedError;
+use async_trait::async_trait;
+use tokio::sync::Mutex;
 
 use super::*;
 
 const RESOURCE: Resource = Resource::User;
 const METHOD: &str = "list_all_users";
+
+#[derive(Default)]
+pub struct ClientMock {
+    pub list_all_users: Option<Arc<dyn Fn(ListAllUsersReq) -> Result<ListAllUsersRes, BoxedError> + Send + Sync>>,
+}
+
+#[async_trait]
+impl ListAllUsersTrait for ClientMock {
+    async fn list_all_users<T>(
+        &self,
+        params: T,
+    ) -> Result<ListAllUsersRes, BoxedError>
+    where
+        T: Into<ListAllUsersReq> + fmt::Debug + Send {
+            let Some(func) = self.list_all_users.clone() else {
+                panic!("list_all_users not defined for mock client");
+            };
+
+            return func(params.into())
+        }
+}
+
+#[cfg(feature = "mock")]
+#[async_trait]
+pub trait ListAllUsersTrait {
+    async fn list_all_users<T>(
+        &self,
+        params: T,
+    ) -> Result<ListAllUsersRes, BoxedError>
+    where
+        T: Into<ListAllUsersReq> + fmt::Debug + Send;
+}
+
+#[async_trait]
+impl ListAllUsersTrait for Client {
+    // #[tracing::instrument(skip(self))]
+    async fn list_all_users<T>(
+        &self,
+        params: T,
+    ) -> Result<ListAllUsersRes, BoxedError>
+    where
+        T: Into<ListAllUsersReq> + fmt::Debug + Send,
+    {
+        let params = params.into();
+
+        let resp: Response<ListAllUsersRes> = self
+            .get("/users", params)
+            .await?;
+
+        let user_res = handle_response(RESOURCE, METHOD, resp)?;
+
+        Ok(user_res)
+    }
+}
+
+#[cfg(feature = "mock")]
+#[async_trait]
+impl ListAllUsersTrait for Arc<Mutex<Option<Result<ListAllUsersRes, BoxedError>>>> {
+    async fn list_all_users<T>(
+        &self,
+        params: T,
+    ) -> Result<ListAllUsersRes, BoxedError>
+    where
+        T: Into<ListAllUsersReq> + fmt::Debug + Send,
+    {
+        let mut val  =self.lock().await;
+
+        val.take().unwrap()
+    }
+}
 
 impl Client {
     #[tracing::instrument(skip(self))]
